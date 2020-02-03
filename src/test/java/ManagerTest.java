bdf4d1457102852fa132357022be8c64f2d63102
Greg Shanks <greg.shanks@gmail.com>
2020-02-03 18:06:16 +0000

    Employee and Manager methods tested

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -17,4 +17,30 @@ public class ManagerTest {
     public void canGetDeptName(){
         assertEquals("Heehaw", manager.getDeptName());
     }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Craig", manager.getName());
+    }
+
+    @Test
+    public void canGetSalary(){
+        assertEquals(36000, manager.getSalary(), 0.00);
+    }
+
+    @Test
+    public void canGetNationalInsuranceNo(){
+        assertEquals("NP329186G", manager.getNationalInsuranceNo());
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        manager.raiseSalary(1000);
+        assertEquals(37000, manager.getSalary(), 0.00);
+    }
+
+    @Test
+    public void canCalculateBonus(){
+        assertEquals(360, manager.payBonus(), 0.00);
+    }
 }
